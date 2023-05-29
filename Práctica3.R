e0e159659afc4c37433826ab74761ce843f21309
cc374-ual <cc374@inlumine.ual.es>
2023-05-29 14:36:51 +0200

    Subida de ejercicio 1 entero

--- Práctica3.R
+++ Práctica3.R
@@ -23,23 +23,292 @@ datos$NOTAFINALSEPTIEMBRE <- ifelse(datos$NOTAFINALSEPTIEMBRE >= 5, "Aprobado", 
 
 
 #Ejercicio 1
+   
+    #
+    #
+    #Usando nota de práctica 1
+    #
+    #
 
-x=datos[,c("Grupo","Practica1")]
-y=datos[,c("NOTAFINALJUNIO")]
+    #U1 = Nota Junio
+    #U2 = Nota Práctica 1
+    #
+    #H0: La variable U1 es independiente de U2. (hipótesis nula)
+    #H1: La variable U1 es dependiente de U2.
+    #
+    #nivel de significancia alpha=0.5
+    
+    U1=datos[,c("EXAMENJUNIO")]
+    U2=datos[,c("Practica1")]
+    
+    tabla = table(U1,U2)
+    tabla
+    t1=chisq.test(tabla,simulate.p.value=TRUE)
+    t1$p.value
+    
+    
+    # Como el p-valor = 0.4122939 > 0.05 = alpha, con los datos disponibles no se puede
+    # rechazar la hipótesis nula por lo que concluimos que son independientes
+    
+    #
+    #
+    #Usando nota de práctica 2
+    #
+    #
+    
+    #U1 = Nota Junio
+    #U2 = Nota Práctica 2
+    #
+    #H0: La variable U1 es independiente de U2. (hipótesis nula)
+    #H1: La variable U1 es dependiente de U2.
+    #
+    #nivel de significancia alpha=0.5
+    
+    U1=datos[,c("EXAMENJUNIO")]
+    U2=datos[,c("Practica2")]
+    
+    tabla = table(U1,U2)
+    tabla
+    t1=chisq.test(tabla,simulate.p.value=TRUE)
+    t1$p.value
+    
+    
+    # Como el p-valor = 0.04347826 < 0.05 = alpha, con los datos disponibles
+    # podemos rechazar la hipótesis nula por lo que concluimos que son dependientes
+    
+    
+    #
+    #
+    #Usando nota de práctica 3
+    #
+    #
+    
+    #U1 = Nota Junio
+    #U2 = Nota Práctica 3
+    #
+    #H0: La variable U1 es independiente de U2. (hipótesis nula)
+    #H1: La variable U1 es dependiente de U2.
+    #
+    #nivel de significancia alpha=0.5
+    
+    U1=datos[,c("EXAMENJUNIO")]
+    U2=datos[,c("Practica3")]
+    
+    tabla = table(U1,U2)
+    tabla
+    t1=chisq.test(tabla,simulate.p.value=TRUE)
+    t1$p.value
+    
+    
+    # Como el p-valor = 0.01549225 < 0.05 = alpha, con los datos disponibles
+    # podemos rechazar la hipótesis nula por lo que concluimos que son dependientes
+    
+    #
+    #
+    #Usando nota de TotalPrácticas
+    #
+    #
+    
+    #U1 = Nota Junio
+    #U2 = Nota TotalPrácticas
+    #
+    #H0: La variable U1 es independiente de U2. (hipótesis nula)
+    #H1: La variable U1 es dependiente de U2.
+    #
+    #nivel de significancia alpha=0.5
+    
+    U1=datos[,c("EXAMENJUNIO")]
+    U2=datos[,c("TOTALpracticas")]
+    
+    tabla = table(U1,U2)
+    tabla
+    t1=chisq.test(tabla,simulate.p.value=TRUE)
+    t1$p.value
+    
+    
+    # Como el p-valor = 0.04797601 < 0.05 = alpha, con los datos disponibles
+    # podemos rechazar la hipótesis nula por lo que concluimos que son dependientes
+    
+    #
+    #
+    #Usando nota de Cuestionario 1 y 2
+    # 
+    #
+    
+    #U1 = Nota Junio
+    #U2 = Nota Cuestionario1y2
+    #
+    #H0: La variable U1 es independiente de U2. (hipótesis nula)
+    #H1: La variable U1 es dependiente de U2.
+    #
+    #nivel de significancia alpha=0.5
+    
+    U1=datos[,c("EXAMENJUNIO")]
+    U2=datos[,c("Cuestionario1y2")]
+    
+    tabla = table(U1,U2)
+    tabla
+    t1=chisq.test(tabla,simulate.p.value=TRUE)
+    t1$p.value
+    
+    
+    # Como el p-valor = 0.4222889 > 0.05 = alpha, con los datos disponibles
+    # no podemos rechazar la hipótesis nula por lo que concluimos que son independientes
+    
+    #
+    #
+    #Usando nota de Cuestionario 3 y 4
+    # 
+    #
+    
+    #U1 = Nota Junio
+    #U2 = Nota Cuestionario3y4
+    #
+    #H0: La variable U1 es independiente de U2. (hipótesis nula)
+    #H1: La variable U1 es dependiente de U2.
+    #
+    #nivel de significancia alpha=0.5
+    
+    U1=datos[,c("EXAMENJUNIO")]
+    U2=datos[,c("Cuestionario3y4")]
+    
+    tabla = table(U1,U2)
+    tabla
+    t1=chisq.test(tabla,simulate.p.value=TRUE)
+    t1$p.value
+    
+    
+    # Como el p-valor = 0.1614193 > 0.05 = alpha, con los datos disponibles
+    # no podemos rechazar la hipótesis nula por lo que concluimos que son independientes
+    
+    #
+    #
+    #Usando nota de Cuestionario 5 y 6
+    # 
+    #
+    
+    #U1 = Nota Junio
+    #U2 = Nota Cuestionario5y6
+    #
+    #H0: La variable U1 es independiente de U2. (hipótesis nula)
+    #H1: La variable U1 es dependiente de U2.
+    #
+    #nivel de significancia alpha=0.5
+    
+    U1=datos[,c("EXAMENJUNIO")]
+    U2=datos[,c("Cuestionario5y6")]
+    
+    tabla = table(U1,U2)
+    tabla
+    t1=chisq.test(tabla,simulate.p.value=TRUE)
+    t1$p.value
+    
+    
+    # Como el p-valor = 0.0109945 < 0.05 = alpha, con los datos disponibles
+    # podemos rechazar la hipótesis nula por lo que concluimos que son dependientes
 
-table(x,y)
-
-group_by(Grupo)
-#U1 = Nota Junio
-#U2 = Nota del resto de variables
-#
-#H0: La variable U1 es independiente de U2.
-#H1: La variable U1 es dependiente de U2.
-#
-#nivel de significancia alpha=0.5
-
-
-tabla = table(datos$Practica1,datos$NOTAFINALJUNIO)
-tabla
-
-
+    
+    #
+    #
+    #Usando nota de TotalCuestionarios
+    # 
+    #
+    
+    #U1 = Nota Junio
+    #U2 = Nota TotalCuestionarios
+    #
+    #H0: La variable U1 es independiente de U2. (hipótesis nula)
+    #H1: La variable U1 es dependiente de U2.
+    #
+    #nivel de significancia alpha=0.5
+    
+    U1=datos[,c("EXAMENJUNIO")]
+    U2=datos[,c("Totalcuestionarios")]
+    
+    tabla = table(U1,U2)
+    tabla
+    t1=chisq.test(tabla,simulate.p.value=TRUE)
+    t1$p.value
+    
+    
+    # Como el p-valor = 0.0189905 < 0.05 = alpha, con los datos disponibles
+    # podemos rechazar la hipótesis nula por lo que concluimos que son dependientes
+    
+    #
+    #
+    #Usando nota de NOTAFINALJUNIO
+    # 
+    #
+    
+    #U1 = Nota Junio
+    #U2 = Nota Final Junio
+    #
+    #H0: La variable U1 es independiente de U2. (hipótesis nula)
+    #H1: La variable U1 es dependiente de U2.
+    #
+    #nivel de significancia alpha=0.5
+    
+    U1=datos[,c("EXAMENJUNIO")]
+    U2=datos[,c("NOTAFINALJUNIO")]
+    
+    tabla = table(U1,U2)
+    tabla
+    t1=chisq.test(tabla,simulate.p.value=TRUE)
+    t1$p.value
+    
+    
+    # Como el p-valor = 0.000995002 < 0.05 = alpha, con los datos disponibles
+    # podemos rechazar la hipótesis nula por lo que concluimos que son dependientes
+    
+    
+    #
+    #
+    #Usando nota de Examen Septiembre
+    # 
+    #
+    
+    #U1 = Nota Junio
+    #U2 = Nota de Examen Septiembre
+    #
+    #H0: La variable U1 es independiente de U2. (hipótesis nula)
+    #H1: La variable U1 es dependiente de U2.
+    #
+    #nivel de significancia alpha=0.5
+    
+    U1=datos[,c("EXAMENJUNIO")]
+    U2=datos[,c("EXAMENSEPTIEMBRE")]
+    
+    tabla = table(U1,U2)
+    tabla
+    t1=chisq.test(tabla,simulate.p.value=TRUE)
+    t1$p.value
+    
+    
+    # Como el p-valor = 1 > 0.05 = alpha, con los datos disponibles
+    # no podemos rechazar la hipótesis nula por lo que concluimos que son independientes
+    
+    #
+    #
+    #Usando nota Final de Septiembre
+    # 
+    #
+    
+    #U1 = Nota Junio
+    #U2 = Nota Final Septiembre
+    #
+    #H0: La variable U1 es independiente de U2. (hipótesis nula)
+    #H1: La variable U1 es dependiente de U2.
+    #
+    #nivel de significancia alpha=0.5
+    
+    U1=datos[,c("EXAMENJUNIO")]
+    U2=datos[,c("NOTAFINALSEPTIEMBRE")]
+    
+    tabla = table(U1,U2)
+    tabla
+    t1=chisq.test(tabla,simulate.p.value=TRUE)
+    t1$p.value
+    
+    
+    # Como el p-valor = 0.5987006 > 0.05 = alpha, con los datos disponibles
+    # no podemos rechazar la hipótesis nula por lo que concluimos que son independientes
